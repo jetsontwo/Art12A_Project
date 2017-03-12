c46d817ea5474ddeb94b20d9ed5ad4ad47f68f9a
jetsontwo <jetsontwo@gmail.com>
2017-03-11 16:18:31 -0800

    Added Pause Button to Quit

---
+++ Assets/Scripts/Pause_Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_Controller : MonoBehaviour {
+
+    public GameObject pause_panel;
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+                Time.timeScale = 0;
+            else
+                Time.timeScale = 1;
+            pause_panel.SetActive(!pause_panel.activeSelf);
+        }
+	}
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
